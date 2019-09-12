a94d9a2708ae2c862f98d96aecb87592e482b975
Natsios Konstantinos <knatsios@trading-point.com>
2019-09-12 15:15:09 +0300

    user error

--- lib/fastlane/plugin/test_center/actions/tests_from_xctestrun.rb
+++ lib/fastlane/plugin/test_center/actions/tests_from_xctestrun.rb
@@ -17,13 +17,12 @@ module Fastlane
 
 
         xctestrun.each do |testable_name, xctestrun_config|
-
+          UI.verbose("testable_name : #{testable_name}")
+          UI.verbose("xctestrun_config : #{xctestrun_config}")
           next if ignoredTestables.include? testable_name
 
           xctest_path = xctest_bundle_path(xctestrun_rootpath, xctestrun_config)
-          UI.verbose("xctest_path : #{xctest_path}")
           test_identifiers = XCTestList.tests(xctest_path)
-          UI.verbose("test_identifiers : #{test_identifiers}")
           UI.verbose("Found the following tests: #{test_identifiers.join("\n\t")}")
 
           if xctestrun_config.key?('SkipTestIdentifiers')
